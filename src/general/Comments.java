ca63908b5c55861056854a1b843029c9d70dfe2d
ewa <eslota@student.wszib.edu.pl>
2023-10-28 23:24:19 +0200

    general package added

---
+++ src/general/Comments.java
@@ -0,0 +1,21 @@
+package general;
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+public class Comments {
+
+
+    public static void main(String[] args) {
+//        System.out.println("O komentarzach...");
+    }
+
+    /*public void test() {
+
+    }*/
+}
--- src/general/HelloWorld.java
+++ src/general/HelloWorld.java
@@ -1,4 +1,4 @@
-package oop;
+package general;
 
 public class HelloWorld {
     public static void main(String[] args) {
