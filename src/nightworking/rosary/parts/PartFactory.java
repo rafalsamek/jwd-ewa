1f5a2070b0670401f69f0676461bce578a3fdaac
Rafal Samek <rsamek@datto.com>
2023-11-09 01:20:08 +0100

    nightworking.rosary: PartFactory - switch changed to switch expression

--- src/nightworking/rosary/parts/PartFactory.java
+++ src/nightworking/rosary/parts/PartFactory.java
@@ -2,17 +2,11 @@ package nightworking.rosary.parts;
 
 public class PartFactory {
     public static Part create(Parts name) throws Exception {
-        switch (name) {
-            case JOYFUL:
-                return new JoyfulPart();
-            case LUMINOUS:
-                return new LuminousPart();
-            case SORROWFUL:
-                return new SorrowfulPart();
-            case GLORIOUS:
-                return new GloriousPart();
-            default:
-                throw new Exception("Unknown Part");
-        }
+        return switch (name) {
+            case JOYFUL -> new JoyfulPart();
+            case LUMINOUS -> new LuminousPart();
+            case SORROWFUL -> new SorrowfulPart();
+            case GLORIOUS -> new GloriousPart();
+        };
     }
 }
